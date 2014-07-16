cafb3bc1e072202336a1adddf8beb4f15d9989fc
CJ Avilla <cjavilla@gmail.com>
2014-07-16 14:36:28 -0700

    setting up backbone skeleton

---
+++ app/assets/javascripts/trello_clone.js
@@ -0,0 +1,13 @@
+window.TrelloClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  TrelloClone.initialize();
+});
